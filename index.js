246124332f7826bb727f7d9d98d4b79ee5de5b89
wblankenship <william.jblankenship@gmail.com>
2016-03-23 00:48:15 -0400

    Increased code quality
    * Added code climate config
    * Added eslint config based on the Airbnb style guide
    * Updated code to match eslint styleguide

--- index.js
+++ index.js
@@ -10,27 +10,30 @@
  * against `process.on('[SIGNAL]'..` to ensure another handler doesn't call
  * `process.exit` before your listener gets a chance to log shutdown.
  */
-var events = require('events')
+'use strict'
+const events = require('events')
 
 // Expose the function
-module.exports = function(eventEmitter, event,  listener) {
+module.exports = function addListenerFirst (eventEmitter, event, listener) {
   // If eventEmitter doesn't have EventEmitter on its protocol chain, then
   // we have nothing to do.
-  if(! (eventEmitter instanceof events.EventEmitter) ) {
+  if (!(eventEmitter instanceof events.EventEmitter)) {
     return null
   }
 
   // Lets get a list of the current listeners to add back
-  var listeners = eventEmitter.listeners(event)
+  const listeners = eventEmitter.listeners(event)
 
   // Lets remove them
   eventEmitter.removeAllListeners(event)
 
   // And add our listener back
-  eventEmitter.on(event,listener)
+  eventEmitter.on(event, listener)
 
   // Then replace all the removed listeners
-  for(var i = 0; i < listeners.length; i++) {
-    eventEmitter.on(event,listeners[i])
+  for (let i = 0; i < listeners.length; i++) {
+    eventEmitter.on(event, listeners[i])
   }
+
+  return null
 }
--- tests/all.js
+++ tests/all.js
@@ -1,50 +1,54 @@
-var addListenerFirst = require('../index.js')
-var EventEmitter = require('events')
-var test = require('tape')
+'use strict'
+
+const addListenerFirst = require('../index.js')
+const EventEmitter = require('events')
+const test = require('tape')
 
 // This function doesn't need to be async because EventEmitter doesn't yield
 // the event loop, meaning all emitters will be run syncronously before the
 // calling context continues execution
-test('Listeners maintain proper order', function(t) {
+test('Listeners maintain proper order', function testOne (t) {
   // We create 3 listeners. We add one directly then the other two in reverse
   // order using `addListenerFirst` and ensure they are called in the correct
   // order.
+
+  // listenersTriggered keeps track of the order listeners are called in
+  let listenersTriggered = 0
+
+  // We expect to see 3 assertions
   t.plan(3)
 
-  //listeners_triggered keeps track of the order listeners are called
-  // in
-  var listeners_triggered = 0
   // Functions are named by the order they should be called, not the order
   // they are added
   function first () {
     // post-increment and assert we are the first called
-    t.equal(listeners_triggered++,0,'first is called first')
+    t.equal(listenersTriggered++, 0, 'first is called first')
   }
 
   function second () {
     // post-increment and assert we are the second called
-    t.equal(listeners_triggered++,1,'second is called second')
+    t.equal(listenersTriggered++, 1, 'second is called second')
   }
 
   function third () {
     // post-increment and assert we are the third called
-    t.equal(listeners_triggered++,2,'third is called third')
+    t.equal(listenersTriggered++, 2, 'third is called third')
   }
 
   // Create a new EventEmitter and attach all of our events to it
-  var emitter = new EventEmitter()
+  const emitter = new EventEmitter()
 
   // Define the event we will respond to
-  var event = 'WOO'
+  const event = 'WOO'
   emitter.on(event, third)
-  addListenerFirst(emitter,event,second)
-  addListenerFirst(emitter,event,first)
+  addListenerFirst(emitter, event, second)
+  addListenerFirst(emitter, event, first)
   emitter.emit(event)
 })
 
 // We want to ensure that this library  can add an event listener on an
 // EventEmitter since it could be a potential edge case.
-test('addListenerFirst works on empty emitter', function(t) {
+test('addListenerFirst works on empty emitter', function testTwo (t) {
   // We need to make sure the function is called
   t.plan(1)
   function toBeCalled () {
@@ -52,8 +56,8 @@ test('addListenerFirst works on empty emitter', function(t) {
   }
 
   // Create a new emitter, add it, and then trigger the event
-  var emitter = new EventEmitter()
-  var event = 'WOO'
+  const emitter = new EventEmitter()
+  const event = 'WOO'
   emitter.on(event, toBeCalled)
   emitter.emit(event)
 })
@@ -62,20 +66,20 @@ test('addListenerFirst works on empty emitter', function(t) {
 // this module does not crash the system. By design, this module will not throw
 // and simply returns null if an invalid object is passed in, no attempt to
 // work with the object will be made
-test('Handle invalid object like a champ', function(t) {
+test('Handle invalid object like a champ', function testThree (t) {
+  // Lets create a fake event listener, then check to see if the module throws
+  // or if it attempts to add a listener to the object.
+  const falseEmitter = {}
 
   // We create a single function stub that should never be called according to
   // this modules contract. We will use this to enssure our function exits
   // as intended
-  function doNotCall() {
+  function doNotCall () {
     t.fail('Should not have attempted to add a listener')
     // Allow for chaining just in case
     return falseEmitter
   }
 
-  // Lets create a fake event listener, check to see if the module throws
-  // or if it attempts to add a listener to the object.
-  var falseEmitter = {}
   falseEmitter.on = doNotCall
   falseEmitter.once = doNotCall
   falseEmitter.listeners = doNotCall
@@ -85,7 +89,7 @@ test('Handle invalid object like a champ', function(t) {
   falseEmitter.setMaxListeners = doNotCall
   falseEmitter.listenerCount = doNotCall
   falseEmitter.removeAllListeners = doNotCall
-  addListenerFirst(falseEmitter, 'WOO', function() {
+  addListenerFirst(falseEmitter, 'WOO', function shouldntBeCalled () {
     t.fail('This function should not be called, something strange has happend')
   })
   t.end()
